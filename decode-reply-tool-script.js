37cff411606f439d3af5cdb516cd91c1e284254f
Scott Smith <mail@ScottHSmith.com>
2013-08-21 16:10:17 -0500

    Ignore gitignore

--- decode-reply-tool-script.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($){
-	$('.replytrigger').click(function(){
-		$('.triggered').removeClass('triggered');
-		$(this).closest('.decode-reply-tool-plugin').addClass('triggered');
-	});
-});
